c4a803cfc86564eef4ec226ff06584a11da4a210
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2021-02-17 14:47:52 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,19 +1,76 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "mepratichhya@outlook.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+<?php
+  
+    require ("class.phpmailer.php");
+  
+    if(isset($_POST['submit'])){
+        $name=$_POST['name']; // Get Name value from HTML Form
+        $mobile=$_POST['mobile'];  // Get Mobile No
+        $email=$_POST['email'];  // Get Email Value
+        $message=$_POST['message']; // Get Message Value
+          
+          
+        $mail = new PHPMailer();
+          
+        $mail->IsSMTP();
+        $mail->Host = "websapex.com"; // Your Domain Name
+          
+        $mail->SMTPAuth = true;
+        $mail->Port = 587;
+        $mail->Username = "info@websapex.com"; // Your Email ID
+        $mail->Password = "DT~-RQyJlaFV"; // Password of your email id
+          
+        $mail->From = "info@websapex.com";
+        $mail->FromName = "WebsApex Team";
+        $mail->AddAddress ("info@websapex.com"); // On which email id you want to get the message
+        $mail->AddCC ($email);
+          
+        $mail->IsHTML(true);
+          
+        $mail->Subject = "Enquiry from Website submitted by $name"; // This is your subject
+          
+        // HTML Message Starts here
+          
+        $mail->Body = "
+        <html>
+            <body>
+                <table style='width:600px;'>
+                    <tbody>
+                        <tr>
+                            <td style='width:150px'><strong>Name: </strong></td>
+                            <td style='width:400px'>$name</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Email ID: </strong></td>
+                            <td style='width:400px'>$email</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Mobile No: </strong></td>
+                            <td style='width:400px'>$mobile</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Message: </strong></td>
+                            <td style='width:400px'>$message</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+        </html>
+        ";
+        // HTML Message Ends here
+          
+              
+        if(!$mail->Send()) {
+            // Message if mail has been sent
+            echo "<script>
+                alert('Submission failed.');
+            </script>";
+        }
+        else {
+            // Message if mail has been not sent
+            echo "<script>
+                alert('Email has been sent successfully.');
+            </script>";
+        }
+  
     }
 ?>
